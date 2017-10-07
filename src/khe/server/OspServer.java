a60cef8d687667c49b17488250ba4b5730481f8a
Emilio Lopez <eil11@case.edu>
2017-10-07 10:36:27 -0400

    Create basic, serial server and start thread

--- src/khe/server/OspServer.java
+++ src/khe/server/OspServer.java
@@ -1,6 +1,10 @@
 package khe.server;
 
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * This class will handle the server operations for Osprey.
  *
@@ -9,4 +13,66 @@ public class OspServer {
     /* Create and bind sockets to listen for the signal
        When it gets the signal, checks if in SHLEEP mode
        If not in SHLEEP, send an email to a configured address */
+
+    // Server behavior
+    private int portNumber;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+
+    // Email behavior
+    private int sleepTimer;
+    private String[] emailRecip;    // This should be read from config file
+
+
+    public static void main(String[] args) {
+
+        /* USAGE
+           java OspServer <PORT_NUMBER> <SLEEP_TIMER>
+         */
+        final String usage = "USAGE: java OspServer <PORT_NUMBER> <SLEEP_TIMER>";
+
+        if (args.length != 2) {
+            System.err.println(usage);
+            System.exit(1);
+        }
+
+        ServerSocket serverSocket;
+        Socket clientSocket;
+
+        int portNumber = Integer.parseInt(args[0]);
+        int sleepTimer = Integer.parseInt(args[1]);
+
+
+        // Keep server running
+        while (true) {
+            String[] emailRecip;
+
+            try {
+                serverSocket = new ServerSocket(portNumber);
+                portNumber = portNumber;
+                clientSocket = serverSocket.accept();    // Waits until a connection is made
+            }
+            catch (IOException q) {
+                System.exit(1);
+            }
+
+
+
+            // After the connection is established and the socket receives a signal
+            // Send email to configured recipients to tell them to shut up
+
+
+            // Avoid spamming emails
+            try {
+                Thread.sleep(sleepTimer * 1000 * 60);
+            }
+            catch (InterruptedException e) {
+                System.out.println("Thread interrupted!");
+            }
+        }
+
+
+
+
+    }
 }
---
+++ src/khe/server/OspThread.java
@@ -0,0 +1,8 @@
+package khe.server;
+
+public class OspThread implements Runnable {
+
+    public void run() {
+
+    }
+}
