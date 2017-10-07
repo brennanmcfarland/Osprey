1811acfb1a0e8a6af45609e55b752bf7107895bd
Emilio Lopez <eil11@case.edu>
2017-10-07 09:30:43 -0400

    Initial commit

---
+++ src/khe/server/OspServer.java
@@ -0,0 +1,12 @@
+package khe.server;
+
+
+/**
+ * This class will handle the server operations for Osprey.
+ *
+ */
+public class OspServer {
+    /* Create and bind sockets to listen for the signal
+       When it gets the signal, checks if in SHLEEP mode
+       If not in SHLEEP, send an email to a configured address */
+}
